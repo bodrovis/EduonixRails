47813b715f3fbf6abf66b083d759280437dd7955
bodrovis <ibodrov@campaigner.ru>
2017-03-10 18:03:36 +0300

    lesson 3.2

--- namespace.rb
+++
@@ -1,9 +0,0 @@
-module FancyOutputter
-  class Printer
-    def initialize
-      puts 'Printer created!'
-    end
-  end
-end
-
-printer = FancyOutputter::Printer.new
--- procs_lambdas.rb
+++
@@ -1,6 +0,0 @@
-def my_method(name, &block)
-  block.call(name)
-end
-my_proc = Proc.new {|string| puts string}
-my_method('test', &my_proc)
-
