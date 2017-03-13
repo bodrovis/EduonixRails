d2bba73800aa6e8ddd34a738d74a75f1749a4eb3
bodrovis <ibodrov@campaigner.ru>
2017-03-13 20:34:43 +0300

    lesson 4.2

--- BraveCoder/app/controllers/pages_controller.rb
+++ BraveCoder/app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   #layout 'admin', only: [:index]
   def index
+    @link = {content: 'our catalog', url: '/catalog'}
+  end
+
+  def about
+    @link = {content: 'our promo page', url: '/promo'}
   end
 end
