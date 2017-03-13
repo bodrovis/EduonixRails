a0890cb0421f94be6753d2604f4331dc370e2865
bodrovis <ibodrov@campaigner.ru>
2017-03-13 20:59:40 +0300

    lesson 4.4

--- BraveCoder/app/controllers/pages_controller.rb
+++ BraveCoder/app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
-  #layout 'admin', only: [:index]
   def index
-    @link = {content: 'our catalog', url: '/catalog'}
   end
 
   def about
-    @link = {content: 'our promo page', url: '/promo'}
   end
 end
--- BraveCoder/app/helpers/application_helper.rb
+++ BraveCoder/app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def title(content)
     content_for(:page_title) {content}
   end
+
+  def at_page(page = '')
+    render 'shared/menu', page: page
+  end
+
+  def nav_item(title, url, current_page = '')
+    content_tag :li, class: "nav-item #{'active' if current_page == title}" do
+      link_to title.titleize, url, class: 'nav-link'
+    end
+  end
 end
