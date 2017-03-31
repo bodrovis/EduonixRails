9dee0908d06a6b146afb52563efe3404c46b2248
bodrovis <ibodrov@campaigner.ru>
2017-03-31 18:59:17 +0300

    lesson 6.3

--- BraveCoder/app/controllers/application_controller.rb
+++ BraveCoder/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound do
+    flash[:danger] = 'Cannot find this record...'
+    redirect_to root_path
+  end
 end
--- BraveCoder/app/controllers/categories_controller.rb
+++ BraveCoder/app/controllers/categories_controller.rb
@@ -1,4 +1,34 @@
 class CategoriesController < ApplicationController
+  before_action :set_category!, only: [:edit, :update, :show, :destroy]
+
+  def destroy
+    if @category.destroy
+      flash[:success] = "Category #{@category.title} removed!"
+    else
+      flash[:danger] = "Cannot remove category #{@category.title}!"
+    end
+    redirect_to categories_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update_attributes(category_params)
+      flash[:success] = "Category #{@category.title} updated!"
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @categories = Category.order(created_at: :desc)
+  end
+
   def new
     @category = Category.new
   end
@@ -7,7 +37,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     if @category.save
       flash[:success] = 'Category added!'
-      redirect_to root_path
+      redirect_to categories_path
     else
       render :new
     end
@@ -17,5 +47,9 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:title, :description)
+  end
+
+  def set_category!
+    @category = Category.find(params[:id])
   end
 end
